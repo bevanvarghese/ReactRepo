f2d66e1ea7b0d95cdb9bfd81f8ca47e878b6848b
bevanvarghese <bevanvar@connect.hku.hk>
2020-07-31 15:50:09 +0530

    Fetch from server

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -19,6 +19,7 @@ import {
 import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
 import { Loading } from "./LoadingComponent";
+import { baseUrl } from "../shared/baseUrl";
 
 function RenderComments({ comments, addComment, dishId }) {
   if (comments != null) {
@@ -48,7 +49,7 @@ function RenderDish({ dish }) {
   if (dish != null) {
     return (
       <Card>
-        <CardImg top src={dish.image} alt={dish.name} />
+        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
         <CardBody>
           <CardTitle>
             <b>{dish.name}</b>
